d39b2f7936120d68f4f6ec53f563c6d68745bd3d
Edgar <ejgobiam@gmail.com>
2024-12-16 15:24:53 -0400

    version_1
    Primer commit.

---
+++ cuenta_lunes.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Este programa calcula la cantidad de lunes entre dos fechas
+
+"""
+
+from datetime import datetime, timedelta
+
+fecha_ini = datetime(2024, 11, 1)
+fecha_fin = datetime(2024, 11, 30)
+
+num_dias_mes = (fecha_fin - fecha_ini).days + 1
+dia_1 = fecha_ini.weekday()
+num_lunes = 0
+
+# 0: lunes; 1:martes; ...
+
+for i in range(num_dias_mes):
+    if ((fecha_ini + timedelta(days = i)).weekday()) == 0:
+        num_lunes = num_lunes + 1
+        
+print(num_lunes)
+
+
+4
+
+ n = input("Indique valor  ")
+print(n)
+
+
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+html = urlopen('https://www.bcv.org.ve/')
+
+bcvBS = BeautifulSoup(html, 'html.parser')
+
+cambio = bcvBS.find('div', {'id':'dolar'}).find('strong').get_text()
+
+print(cambio)
+
